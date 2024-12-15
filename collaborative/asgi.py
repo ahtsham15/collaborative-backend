9aad96de7ed6ca49d44322a2819a343cc8f7b3c4
Ahtsham <ahtshamsandhu05@gmail.com>
2024-12-15 20:38:50 +0500

    tasks/consumers: configure the channels dependencies

--- collaborative/asgi.py
+++ collaborative/asgi.py
@@ -1,34 +1,21 @@
-"""
-ASGI config for collaborative project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-# import os
-
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborative.settings')
-
-# application = get_asgi_application()
-
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from tasks.consumers import TestConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_list.settings')
 
+django_asgi_app = get_asgi_application()
+
+ws_patterns = [
+    path("ws/test/", TestConsumer.as_asgi()),
+]
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # "websocket": AuthMiddlewareStack(
-    #     URLRouter(
-    #         # Your routing for WebSocket
-            
-    #     )
-    # ),
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(  # Wrap WebSocket connections with authentication
+        URLRouter(ws_patterns)
+    ),
 })
